c051cb73bb5ea7a5625f44f48bc45c97f14917fa
egemenelz <egemenelz@gmail.com>
2022-08-06 18:17:54 -0400

    Updates

---
+++ precache-manifest.096d7c5bd2dd39f4b275c1b050244dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca7846f223b8a3a0038d",
+    "url": "/portfolio-v1.1/static/css/main.aefed85c.chunk.css"
+  },
+  {
+    "revision": "ca7846f223b8a3a0038d",
+    "url": "/portfolio-v1.1/static/js/main.ca7846f2.chunk.js"
+  },
+  {
+    "revision": "43ce605ecad8a4ab63dc",
+    "url": "/portfolio-v1.1/static/js/1.43ce605e.chunk.js"
+  },
+  {
+    "revision": "6333821e88f6b4cd0226",
+    "url": "/portfolio-v1.1/static/js/runtime~main.6333821e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio-v1.1/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "667bbec08a828e00c3f36d07ac35e435",
+    "url": "/portfolio-v1.1/index.html"
+  }
+];
